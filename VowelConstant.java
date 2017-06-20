eee4e2e695da7f35c88b98fe1c0a49bcce9a7fda
PadmaAlwar <padmaalwa1996@gmail.com>
2017-06-20 16:41:29 +0530

    Create VowelConstant.java

---
+++ VowelConstant.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+import java.util.*;
+import java.lang.*;
+import java.io.*; 
+ public class VowelConstant {
+	 public static void main(String[] args) {
+		 char ch;
+		 Scanner obj=new Scanner(System.in);
+		 System.out.println("Enter an alphabet:");
+	     ch=obj.next().charAt(0);
+	     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	     {
+	    	 System.out.println("Entered character"+ch+"is a vowel");
+	     }
+	     else
+	     {
+	    	 System.out.println("Entered character  '"+ch+"' is a consonant");
+	    	 
+	     }
+	 }
+	  
+}
